ea586173d58709b6c506e5ea577c0f971d0c0a1e
Torben Hohn <torbenh@gmx.de>
2010-04-03 08:00:12 +0200

    more dialogs and functional popup menu for jacksmpanel

--- src/jacksmpanel.py
+++ src/jacksmpanel.py
@@ -5,26 +5,35 @@ import dbus.service
 import pyjacksm
 import os
 
-class dialog1(object):
-    def __init__( self ):
-	builder = gtk.Builder()
-	builder.add_from_file( os.path.join( pyjacksm.__path__[0], "data", "jacksmpanel.glade" ))
+class builder_base( gtk.Builder ):
+    def __init__( self, filename ):
+	gtk.Builder.__init__( self )
+	self.add_from_file( os.path.join( pyjacksm.__path__[0], "data", filename ))
 
-	self.dialog = builder.get_object( "dialog1" )
-	self.treeview = builder.get_object( "treeview1" )
-	self.listmodel = builder.get_object( "liststore1" )
+
+class dialog1( object ):
+    def __init__( self ):
+	bb = builder_base( "load_dialog.glade" )
+
+	self.dialog    = bb.get_object( "dialog1" )
+	self.treeview  = bb.get_object( "treeview1" )
+	self.listmodel = bb.get_object( "liststore1" )
+
+	bb.connect_signals( self, None )
 
 	self.treeview.append_column( gtk.TreeViewColumn( "name", gtk.CellRendererText(), text=0 ) )
 
 	projects = sm_iface.list()
+	projects.sort()
 
 	for i in projects:
 	    self.listmodel.append( [i] )
 
-	builder.connect_signals( self, None )
+
+	self.dialog.show()
 	    
+
     def ok_clicked( self, data ):
-	print "ok"
 	sel = self.treeview.get_selection()
 	(model, it) = sel.get_selected()
 	if not it:
@@ -33,18 +42,81 @@ class dialog1(object):
 	value = model.get_value( it, 0 )
 
 	sm_iface.load( value )
-
+	self.dialog.destroy()
 
     def cancel_clicked( self, data ):
 	self.dialog.destroy()
-	print "cancel"
+
+class save_dialog( object ):
+    def __init__( self ):
+	bb = builder_base( "save_dialog.glade" )
+
+	self.dialog    = bb.get_object( "save_dialog" )
+	self.treeview  = bb.get_object( "save_tree" )
+	self.listmodel = bb.get_object( "liststore1" )
+	self.entry     = bb.get_object( "name_entry" )
+
+	bb.connect_signals( self, None )
+
+	self.treeview.append_column( gtk.TreeViewColumn( "name", gtk.CellRendererText(), text=0 ) )
+
+	projects = sm_iface.list()
+	projects.sort()
+
+	for i in projects:
+	    self.listmodel.append( [i] )
+
+
+	self.dialog.show()
+
+    def tree_sel( self, data ):
+	sel = self.treeview.get_selection()
+	(model, it) = sel.get_selected()
+	if not it:
+	    return
+
+	value = model.get_value( it, 0 )
+	print value
+	self.entry.set_text( value )
+
+
+    def ok_clicked( self, data ):
+	print self.entry.get_text()
+	self.dialog.destroy()
+
+    def cancel_clicked( self, data ):
+	self.dialog.destroy()
+
+
+class popup_menu(object):
+    def __init__( self, button, activate_time ):
+	bb = builder_base( "session_popup.glade" )
+
+	self.menu = bb.get_object( "popup_menu" )
+	bb.connect_signals( self, None )
+
+	self.menu.popup ( None, None, None, button, activate_time )
+
+    def save_cb( self, data ):
+	print "save"
+
+    def save_as_cb( self, data ):
+	d = save_dialog()
+	print "save as"
+
+    def load_cb( self, data ):
+	print "load"
+
+    def quit_cb( self, data ):
+	print "quit"
 
 
 def icon_activate( icon ):
+    d = dialog1()
 
-    d = dialog1() 
+def icon_popup( icon, button, activate_time ):
+    p = popup_menu( button, activate_time )
 
-    d.dialog.show()
 
 
 def get_sm_iface():
@@ -56,9 +128,9 @@ def get_sm_iface():
 
 sm_iface = get_sm_iface()
 
-
 sicon = gtk.status_icon_new_from_file( os.path.join( pyjacksm.__path__[0], "data", "jack_sm_icon.png" ))
 
-sicon.connect( "activate", icon_activate )
+#sicon.connect( "activate", icon_activate )
+sicon.connect( "popup-menu", icon_popup )
 
 gtk.main()
