98a4a45af4fca4a1965ac0720a6741b725dc6a5a
Torben Hohn <torbenh@gmx.de>
2010-04-03 13:01:42 +0200

    remove the old single glade file

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='pyjacksm',
 	url='http://www.jackaudio.org/',
 	packages=['pyjacksm'],
 	package_dir  = {'pyjacksm': 'src/pyjacksm'},
-	package_data = {'pyjacksm': ["data/jacksmpanel.glade", "data/jack_sm_icon.png"] },
+	package_data = {'pyjacksm': ["data/*.glade", "data/jack_sm_icon.png"] },
 	scripts=['src/jacksm'],
 	data_files=[('/usr/share/dbus-1/services', ['data/org.jackaudio.sessionmanager.service'])]
 	)
